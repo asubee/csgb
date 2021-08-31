b0c3a25916805cd299b3e44e68a9132f6df28943
EC2 Default User <ec2-user@ip-172-31-32-88.ap-northeast-1.compute.internal>
2021-08-31 12:28:15 +0000

    backup :20210831_122815

---
+++ ggp/cocoon-child-master/functions.php
@@ -0,0 +1,16 @@
+<?php //子テーマ用関数
+if ( !defined( 'ABSPATH' ) ) exit;
+
+//子テーマ用のビジュアルエディタースタイルを適用
+add_editor_style();
+
+//以下に子テーマ用の関数を書く
+function mytheme_admin_enqueue() {
+    // cssファイルを追加
+    wp_enqueue_style( 'my_admin_css', get_stylesheet_directory_uri() . '/my-admin-style.css' );
+}
+add_action( 'admin_enqueue_scripts', 'mytheme_admin_enqueue' );
+
+require_once(get_stylesheet_directory().'/lib/ggp_setup.php');
+require_once(get_stylesheet_directory().'/lib/db.php');
+
